0a1ca7f5548445c96cf2761f400387b5068fd512
Sonjoy <77813705+leosonjoy@users.noreply.github.com>
2022-12-25 06:27:36 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,7 +24,8 @@ async function mealSearch(mealName) {
   const res = await fetch(url);
   const resData = await res.json();
   const meals = await resData.meals;
-
+  
+  searchItemContainer.innerHTML='';
   searchCon.innerHTML = ""; //search container reset
   const h1 = document.createElement("h1");
   h1.className = "text-center";
